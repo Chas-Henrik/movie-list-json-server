1703e694fc99d9afdd627bed16706c4567961ce0
Henrik Suurik <henrik.suurik@chasacademy.se>
2024-12-02 10:55:46 +0100

    Improved error handling

--- src/main.js
+++ src/main.js
@@ -9,9 +9,16 @@ const titleInputElement = document.getElementById('title-id');
 const ratingInputElement = document.getElementById('rating-id');
 const pagePrevBtnElement = document.getElementById('page-prev-btn-id');
 const pageNextBtnElement = document.getElementById('page-next-btn-id');
+const pageResetBtnElement = document.getElementById('reset-btn-id');
+
+const State = Object.freeze({ 
+    EMPTY: 1,
+    FULL: 2,
+});
 
 let currentPage = 1;
 let lastPage = 1;
+let state = State.EMPTY;
 
 createBtnElement.addEventListener('click', createClick);
 updateBtnElement.addEventListener('click', updateClick);
@@ -19,13 +26,30 @@ moveListContainerElement.addEventListener('click', selectMovieClick);
 sortedCheckboxElement.addEventListener('change', refreshMovieList);
 pagePrevBtnElement.addEventListener('click', getPrevPage);
 pageNextBtnElement.addEventListener('click', getNextPage);
+pageResetBtnElement.addEventListener('click', resetForm);
+
+function setState(newState, param = null) {
+    state = newState;
+    switch(state) {
+        case State.EMPTY:
+            formElement.reset();
+            updateBtnElement.disabled = true;
+            pageResetBtnElement.disabled = true;
+            break;
+        case State.FULL:
+            updateBtnElement.dataset.id = param;
+            updateBtnElement.disabled = false;
+            pageResetBtnElement.disabled = false;
+            break;
+    }
+}
 
 async function createClick(event) {
-    const data = await httpPost(titleInputElement.value, ratingInputElement.value);
-    refreshMovieList();
-    updateBtnElement.dataset.id = data.id;
-    updateBtnElement.classList.remove("collapsed");
-    event.preventDefault();
+    if(titleInputElement.value != '' && ratingInputElement.value != '') {
+        const data = await httpPost(titleInputElement.value, ratingInputElement.value);
+        refreshMovieList();
+        setState(State.FULL, data.id);
+    }
 }
 
 async function updateClick(event) {
@@ -45,16 +69,14 @@ async function selectMovieClick(event) {
             movieContainerElement = clickedElement.parentElement;
             await httpDeleteId(movieContainerElement.dataset.id);
             refreshMovieList();
-            formElement.reset();
-            updateBtnElement.classList.add("collapsed");
+            setState(State.EMPTY);
             break;
         case 'P':
             movieContainerElement = clickedElement.parentElement;
         default:
             titleInputElement.value = movieContainerElement.childNodes[2].innerText;
             ratingInputElement.value = movieContainerElement.childNodes[1].innerText;
-            updateBtnElement.dataset.id = movieContainerElement.dataset.id;
-            updateBtnElement.classList.remove("collapsed");
+            setState(State.FULL, movieContainerElement.dataset.id);
             break;
     }
 
@@ -72,6 +94,11 @@ function getNextPage(event) {
     console.log("pagePrev");
     if(currentPage < lastPage) currentPage++;
     refreshMovieList();
+    event.preventDefault();
+}
+
+function resetForm(event) {
+    setState(State.EMPTY);
     event.preventDefault();
 }
 

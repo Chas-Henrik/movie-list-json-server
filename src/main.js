f1c58d83fb8efe464899d329bd1ee2d8c4a01dcd
Henrik Suurik <henrik.suurik@chasacademy.se>
2024-12-02 15:15:17 +0100

    Fixed bugs

--- src/main.js
+++ src/main.js
@@ -3,103 +3,101 @@ import { httpPost, httpPut, httpGet, httpDeleteId } from './json_server_api.js';
 const formElement = document.getElementById('form-id');
 const moveListContainerElement = document.getElementById('movie-list-id'); 
 const sortedCheckboxElement = document.getElementById('sorted-checkbox-id'); 
-const createBtnElement = document.getElementById('create-btn-id');
 const updateBtnElement = document.getElementById('update-btn-id');
+const resetBtnElement = document.getElementById('reset-btn-id');
 const titleInputElement = document.getElementById('title-id');
 const ratingInputElement = document.getElementById('rating-id');
 const pagePrevBtnElement = document.getElementById('page-prev-btn-id');
 const pageNextBtnElement = document.getElementById('page-next-btn-id');
-const pageResetBtnElement = document.getElementById('reset-btn-id');
 
 const State = Object.freeze({ 
-    EMPTY: 1,
-    FULL: 2,
+    RESET: 1,
+    UPDATE: 2,
 });
 
 let currentPage = 1;
 let lastPage = 1;
-let state = State.EMPTY;
+let state = State.RESET;
 
-createBtnElement.addEventListener('click', createClick);
+formElement.addEventListener('submit', submitForm);
+formElement.addEventListener('reset', resetForm);
 updateBtnElement.addEventListener('click', updateClick);
 moveListContainerElement.addEventListener('click', selectMovieClick);
 sortedCheckboxElement.addEventListener('change', refreshMovieList);
 pagePrevBtnElement.addEventListener('click', getPrevPage);
 pageNextBtnElement.addEventListener('click', getNextPage);
-pageResetBtnElement.addEventListener('click', resetForm);
 
 function setState(newState, param = null) {
-    state = newState;
-    switch(state) {
-        case State.EMPTY:
-            formElement.reset();
-            updateBtnElement.disabled = true;
-            pageResetBtnElement.disabled = true;
-            break;
-        case State.FULL:
-            updateBtnElement.dataset.id = param;
-            updateBtnElement.disabled = false;
-            pageResetBtnElement.disabled = false;
-            break;
+    if(newState !== state) {
+        switch(newState) {
+            case State.RESET:
+                formElement.reset();
+                updateBtnElement.dataset.id = null;
+                updateBtnElement.disabled = true;
+                resetBtnElement.disabled = true;
+                break;
+            case State.UPDATE:
+                updateBtnElement.dataset.id = param;
+                updateBtnElement.disabled = false;
+                resetBtnElement.disabled = false;
+                break;
+        }
+        state = newState;
     }
 }
 
-async function createClick(event) {
-    if(titleInputElement.value != '' && ratingInputElement.value != '') {
-        const data = await httpPost(titleInputElement.value, ratingInputElement.value);
-        refreshMovieList();
-        setState(State.FULL, data.id);
-    }
+async function submitForm(event) {
+    //Create Element
+    event.preventDefault();
+    const data = await httpPost(titleInputElement.value, ratingInputElement.value);
+    refreshMovieList();
+    setState(State.UPDATE, data.id);
 }
 
 async function updateClick(event) {
+    //Update Element
+    event.preventDefault();
     const btnElement = event.target;
-    console.log("btnElement.dataset.id", btnElement.dataset.id);
     await httpPut(btnElement.dataset.id, titleInputElement.value, ratingInputElement.value);
     refreshMovieList();
-    event.preventDefault();
+}
+
+function resetForm(event) {
+    //Update Form
+    setState(State.RESET);
 }
 
 async function selectMovieClick(event) {
     let clickedElement = event.target;
-    let movieContainerElement;
+    let movieContainerElement = clickedElement;
 
     switch(clickedElement.tagName) {
         case 'BUTTON':
             movieContainerElement = clickedElement.parentElement;
+            const currentDeleted = (updateBtnElement.dataset.id === movieContainerElement.dataset.id);
             await httpDeleteId(movieContainerElement.dataset.id);
             refreshMovieList();
-            setState(State.EMPTY);
+            if(currentDeleted)
+                setState(State.RESET);
             break;
         case 'P':
             movieContainerElement = clickedElement.parentElement;
-        default:
+        case 'DIV':
             titleInputElement.value = movieContainerElement.childNodes[2].innerText;
             ratingInputElement.value = movieContainerElement.childNodes[1].innerText;
-            setState(State.FULL, movieContainerElement.dataset.id);
+            setState(State.UPDATE, movieContainerElement.dataset.id);
             break;
     }
-
-    event.preventDefault();
 }
 
 function getPrevPage(event) {
-    console.log("pagePrev");
     if(currentPage>1) currentPage--;
     refreshMovieList();
-    event.preventDefault();
 }
 
 function getNextPage(event) {
-    console.log("pagePrev");
     if(currentPage < lastPage) currentPage++;
     refreshMovieList();
-    event.preventDefault();
-}
-
-function resetForm(event) {
-    setState(State.EMPTY);
-    event.preventDefault();
 }
 
 function populateMovieElement(id, title, rating) {
